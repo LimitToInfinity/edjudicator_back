4918686f5400d5050e153f58b147193a4c923b06
github username <github email address>
2019-09-10 14:24:14 -0600

    Rollback High Score model update.

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class HighScore(models.Model):
     value = models.IntegerField(null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    # def __str__(self):
+    #     return "{} - {}".format(self.value, self.user)
     def __str__(self):
-        return "{} - {}".format(self.value, self.user)
+        return "{}".format(self.value)
