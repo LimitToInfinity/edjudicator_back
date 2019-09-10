03b9a5605d722610be9c9f4bc377d81a32e1b3f0
github username <github email address>
2019-09-10 16:58:13 -0600

    Removed user from serializer.

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class HighScore(models.Model):
-    game_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     value = models.IntegerField(default=0)
 
     def __str__(self):
-        return "{} - {}".format(self.value, self.game_user)
+        return "{} - {}".format(self.value, self.user)
