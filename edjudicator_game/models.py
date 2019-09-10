3e36a064a33a25e1dbea0af67503111d6015840b
github username <github email address>
2019-09-10 12:00:00 -0600

    High Score belongs to User.

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class HighScore(models.Model):
     value = models.IntegerField(null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{}".format(self.value)
+        return "{} - {}".format(self.value, self.user)
