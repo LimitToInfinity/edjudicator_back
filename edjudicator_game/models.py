222b47fbafb99e29ec068badc51f1c4dec7f0a11
github username <github email address>
2019-09-11 00:47:16 -0600

    moved import order in models

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -1,11 +1,8 @@
-# from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-User = get_user_model()
 
 # Create your models here.
 class User(AbstractUser):
@@ -14,7 +11,7 @@ class User(AbstractUser):
 
 class HighScore(models.Model):
     user = models.OneToOneField(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         primary_key=True,
     )
