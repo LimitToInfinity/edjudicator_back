d0a8806ba1cdac11da61e74f4982a207e40e58d5
github username <github email address>
2019-09-11 00:42:15 -0600

    get_user_model instead of settings.

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -1,8 +1,11 @@
-from django.conf import settings
+# from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+User = get_user_model()
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,7 +14,7 @@ class User(AbstractUser):
 
 class HighScore(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         primary_key=True,
     )
