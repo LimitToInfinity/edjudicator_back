2c164f289ed53721e0c99267f6535d5f08e347e2
github username <github email address>
2019-09-10 18:18:46 -0600

    Reset databases to postgresql.

--- edjudicator_api/settings.py
+++ edjudicator_api/settings.py
@@ -97,19 +97,19 @@ WSGI_APPLICATION = 'edjudicator_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'edjudicator',
-#         'USER': 'limitless',
-#         'PASSWORD': '123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     },
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'edjudicator',
+        'USER': 'limitless',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
