95294506c119d6f9d6ae15579d4ca4251d4cbb8b
github username <github email address>
2019-09-11 01:51:38 -0600

    debug set to true

--- edjudicator_api/settings.py
+++ edjudicator_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2hz%@w$!1f7h57a!^zyx&%fw(p37_=_ed8zjg%aaty@@(3nw_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
