55e28259dab574f3bae7c26f276c810ce13134e5
github username <github email address>
2019-09-10 22:12:53 -0600

    Changed to onetoone relationship.

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 # Create your models here.
 class HighScore(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        primary_key=True,
+    )
     value = models.IntegerField(default=0)
 
     def __str__(self):
