1e23ec8be51397f0702ac4e49333a58e13c6e1f1
github username <github email address>
2019-09-11 03:16:40 -0600

    https back in settings and serializer for highscore has username and email

--- edjudicator_game/serializers.py
+++ edjudicator_game/serializers.py
@@ -3,13 +3,15 @@ from .models import HighScore
 from django.contrib.auth.models import User
 
 class HighScoreSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username")
+    email = serializers.CharField(source="user.email")
     class Meta:
         model = HighScore
-        fields = ("user", "value")
+        fields = ("user", "username", "email", "value")
 
         def update(self, instance, validated_data):
             instance.value = validated_data.get("value", instance.value)
-            # instance.user = validated_data.get("user", instance.user)
+            instance.user = validated_data.get("user", instance.user)
             instance.save()
             return instance
 
