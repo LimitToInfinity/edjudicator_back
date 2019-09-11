b076aa85383ac98e3c64efde0fdd078c4206171c
github username <github email address>
2019-09-10 18:02:54 -0600

    Global accounts/login updated to auth/login/.

--- edjudicator_game/serializers.py
+++ edjudicator_game/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class HighScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = HighScore
-        fields = ("id", "value")
+        fields = ("id", "user", "value")
 
         def update(self, instance, validated_data):
             instance.value = validated_data.get("value", instance.value)
