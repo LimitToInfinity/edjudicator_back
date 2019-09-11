327230a5b95a3f808cdfb5bc8d3b4f0bbe9cd4bf
github username <github email address>
2019-09-11 01:41:55 -0600

    added receiver and post_save to highscore model

--- edjudicator_game/models.py
+++ edjudicator_game/models.py
@@ -1,14 +1,18 @@
 from django.conf import settings
 from django.db import models
+from django.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class HighScore(models.Model):
-    # user = models.OneToOneField(
-    #     settings.AUTH_USER_MODEL,
-    #     on_delete=models.CASCADE,
-    #     primary_key=True,
-    # )
+    user = models.OneToOneField(
+         settings.AUTH_USER_MODEL,
+         on_delete=models.CASCADE,
+         primary_key=True,
+    )
     value = models.IntegerField(default=0)
 
-    def __str__(self):
-        return "{}".format(self.value)
+@receiver(post_save, sender=User)
+def create_user_score(sender, instance, created, **kwargs):
+    if created:
+        HighScore.objects.create(user=instance)
